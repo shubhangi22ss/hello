914b2c59f31b7e7838fdbaa3d31e65e89783979f
shubhangi22ss <78610563+shubhangi22ss@users.noreply.github.com>
2022-04-29 11:00:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("HELLO")
+# A school method based Python3
+# program to check if a number
+# is prime
+n=11
+def isPrime(n):
+
+	# Corner case
+	if n <= 1:
+		return False
+
+	# Check from 2 to n-1
+	for i in range(2, n):
+		if n % i == 0:
+			return False;
+
+	return True
+
+
